8702c5a8a389a9cff4dd3965a8501e5d4916ce72
ivanvujkc <ivan.vu.cv@gmail.com>
2020-02-21 12:53:35 -0500

    Create 1_unmatched_cohorts_nperms.R

---
+++ 1_unmatched_cohorts_nperms.R
@@ -0,0 +1,100 @@
+# This script creates n permutations of location-matched ('unmatched' for confounding variables) case-control cohorts for a list of conditions.
+# It requires as input a csv file for each condition, with cases being marked as '1' under a column titled 'target'. Such files are the output of python scripts at github.com/jacksklar/AGPMicrobiomeHostPredictions, found in the 'Feature_Cohorts/binary_cohorts_no_matching' output folder. Such files must include all cases that are present in the mapping file.
+
+library(ggplot2);library(labdsv);library(reshape2);library(vegan)
+
+# Enter path to directory containing cohorts to make permutations of: 
+cohortnames <-list.files("~/pathto_casesdirectory",pattern=".csv",full.names=F)
+cohortpaths <-list.files("~/pathto_casesdirectory",pattern=".csv",full.names=T)
+
+# Define number of cohort permutations to create
+nperms<-25
+
+# Define output filepath
+outpath<-"~/pathto_outputdirectory"
+
+tab<-read.table("~/pathto_otutable.txt",sep="\t",header=T,row.names=1)
+
+
+for(h in 1:length(cohortpaths)){
+
+unmatched<-read.csv(file=cohortpaths[h],header=T,row.names=1)
+
+targetu<-rownames(unmatched[unmatched$target==1,])
+
+allmetadat<-read.csv(file="~/Dropbox/post-doc/collaborations/190208_sklar_aging_microbiome/190822_AmericanGutProj_JS2/Data/Cleaned_data/numerical_metadata.csv",header=T,row.names=1)
+locationmetadat <-read.table(file="~/Dropbox/post-doc/collaborations/190208_sklar_aging_microbiome/190822_AmericanGutProj_JS2/Data/Cleaned_data/selection_pop_meta_loc_updated.csv",header=T,row.names=1,sep=",")
+allmetadat2<-allmetadat[,!colnames(allmetadat)%in%c("longitude","latitude")]
+mergmetadat<-merge(allmetadat2, locationmetadat,by="row.names")
+rownames(mergmetadat)<-mergmetadat$Row.names
+
+map1<-mergmetadat
+
+# Impose inclusion/exclusion criteria
+map2<-subset(map1,age_years>=20&age_years<=80&antibiotic_history<2&ibd==0&diabetes==0&bmi>=12.5&bmi<=40)
+map2<-map2[rowSums(map2[,c("country_Canada","country_USA","country_United.Kingdom")])>0,]
+
+# Remove subjects not in OTU/ASV table
+map2<-map2[rownames(map2)%in%colnames(tab),]
+
+# Scale and center all matching variables
+map2$latitude<-scale(map2$latitude)
+map2$longitude<-scale(map2$longitude)
+map2$sex<-scale(map2$sex)
+map2$age_years<-scale(map2$age_years)
+map2$bmi<-scale(map2$bmi)
+map2$alcohol_frequency<-scale(map2$alcohol_frequency)
+map2$vegetable_frequency<-scale(map2$vegetable_frequency)
+map2$milk_cheese_frequency<-scale(map2$milk_cheese_frequency)
+map2$meat_eggs_frequency<-scale(map2$meat_eggs_frequency)
+map2$bowel_movement_quality <-scale(map2$bowel_movement_quality)
+map2$whole_grain_frequency <-scale(map2$whole_grain_frequency)
+map2$sugary_sweets_frequency <-scale(map2$sugary_sweets_frequency)
+map2$salted_snacks_frequency <-scale(map2$salted_snacks_frequency)
+map3<-map2[,c("latitude","longitude","sex","age_years","bmi", "alcohol_frequency","vegetable_frequency", "milk_cheese_frequency", "meat_eggs_frequency","bowel_movement_quality","whole_grain_frequency","sugary_sweets_frequency","salted_snacks_frequency")]
+locvars<-c("longitude","latitude")
+
+map3<-map3[!is.na(rowSums(map3)),]
+
+allmet2<-allmetadat[rownames(map3),]
+
+# Remove all subjects not reporting information for given condition of interest
+allnonnas<-rownames(allmet2[!is.na(allmet2[, dis2[h]]==0),])
+
+# Create mapping file with just location metadata
+tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars)]
+
+# Create Euclidean distance matrix based on location metadata
+eudist<-dist(tempmap[,],method="euclidean")
+eu2<-as.matrix(eudist)
+
+  for (l in 1:nperms)
+  {
+  	# Select only case subjects with matching variable metadata in mapping file
+	target1b<-targetu[targetu%in%rownames(map3)]
+  	# Randomize order of cases
+  	target1<-sample(target1b,size=length(target1b))
+
+  	ftab<-matrix(nrow=length(target1)*2,ncol=2)
+    colnames(ftab)<-c("sampID","target")
+    ftab[1:length(target1),1]<-target1
+    ftab[1:length(target1),2]<-"case"
+    ftab[(length(target1)+1):(length(target1)*2),2]<-"control"
+    for (i in 1:length(target1))
+    {
+      sampname<-target1[i]
+      # Rank controls by shortest Euclidean distance to case subject
+      contsinord<-colnames(eu2[,order(eu2[sampname,])])
+      # Remove from consideration all cases and previously selected controls
+      contsinord2<-contsinord[!contsinord%in%ftab[,1]]
+      # Select top 5 most similar controls for this given case
+      topconts <-contsinord2[1:5]
+      # Randomly select one from top 5 most similar controls
+      randcont<-sample(topconts,size=1)
+      ftab[i+length(target1),1]<-randcont
+    }
+
+rownames(ftab)<-ftab[,"sampID"]
+write.csv(ftab,paste(outpath, cohortnames[h], "_",l, "_unmatched.csv",sep=""))
+}
+}
