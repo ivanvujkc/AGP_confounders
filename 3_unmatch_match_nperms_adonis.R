9204047e18d0a6ff579da100f7926af6f6e51cea
ivanvujkc <ivan.vu.cv@gmail.com>
2020-03-20 18:37:46 -0400

    Update 3_unmatch_match_nperms_adonis.R

--- 3_unmatch_match_nperms_adonis.R
+++ 3_unmatch_match_nperms_adonis.R
@@ -1,16 +1,8 @@
-# The following script performs Adonis (PERMANOVA) tests and thus calculates P values and F statistics for 25 unmatched and 25 confounder variable-matched cohorts for each condition. This script requires assessment of which confounder variables differ between cases and controls (output of "2_fishwilx_unmatched_cohorts.R" script), as well as lists of cases within metadata (input of "1_unmatch_nperms"). N.b. inclusion/exclusion criteria differed in our study for: autism spectrum disease, type 2 diabetes, and inflammatory bowel disease, as written in the manuscript. Below, inclusion/exclusion criteria for all remaining diseases is shown.
+# The following script performs Adonis (PERMANOVA) tests and thus calculates P values and F statistics for 25 unmatched and 25 confounder variable-matched cohorts for each condition. This script requires assessment of which confounder variables differ between cases and controls (output of "2_fishwilx_unmatched_cohorts.R" script), as well as lists of cases within metadata (input of "1_unmatch_nperms"). N.b. inclusion/exclusion criteria differed in our study for: autism spectrum disease, type 2 diabetes, and inflammatory bowel disease, as written in the manuscript. 
 
 library(ggplot2);library(labdsv);library(reshape2);library(vegan)
 
-fishwilx2 <-read.csv(file="~/pathto_outputdirectory_mismatchtest/fishwilx_summary.csv",header=T)
-dis<-unique(fishwilx2$dis)
-
-cohortnames <-list.files("~/pathto_casesdirectory",pattern=".csv",full.names=F)
-cohortpaths <-list.files("~/pathto_casesdirectory",pattern=".csv",full.names=T)
-
-# Ensure that vectors "cohortnames" and "dis" comprise the same list of conditions
-cohortnames%in%dis
-dis%in%cohortnames
+fishwilx2 <-read.csv(file="~/Downloads/outputdirectory/fishmann_unmat/fishmann_summary.csv",header=T)
 
 # Define number of cohort permutations to create
 nperms<-25
@@ -18,34 +10,37 @@ nperms<-25
 # Define maximum cohort size (total number of cases and controls)
 maxcohortcutoff<-1000
 
-# Define number of Adonis (PERMANOVA) permutations to perform for P value calculation (can significantly increase CPU time)
+# Define number of Adonis (PERMANOVA) permutations to perform for P value calculation (higher numbers increase CPU time)
 adonperms<-999
 
 # Define output filepath for case-control cohorts
-outpath<-"~/pathto_outputdirectory_matunmatcohorts/"
+outpathcohorts<-"~/Downloads/outputdirectory/mat_unmat_cohorts/"
 
 # Define output filepath for visualizations and Adonis results
-outpathadon<-"~/pathto_outputdirectory_matunmat_adonis_graphs/"
+outpathadon<-"~/Downloads/outputdirectory/adonis/"
 
 # Define path to input OTU/ASV table, with taxa as rows
-tab<-read.table("~/pathto_otutable.txt",sep="\t",header=T,row.names=1)
+tab<-read.csv("~/Downloads/merged2_s50_otus_mc01p.csv",header=T,row.names=1)
 
 
 adonmatall<-data.frame(matrix(ncol=10))
 colnames(adonmatall)<-c("Df","SumsOfSqs","MeanSqs","Fstat","R2","P_value","sampIDparam","numsamps","distparam","disease")
 
+target1b<-as.vector(read.csv(file="~/Downloads/cases_diabetes.csv")[,1])
 
-for(h in 1:length(cohortnames)){
+map1<-read.csv(file="~/Downloads/metadat.csv",header=T,row.names=1)
 
-unmatched<-read.csv(file=cohortpaths[h],header=T,row.names=1)
-
-targetu<-rownames(unmatched[unmatched$target==1,])
-
-map1<-read.csv(file="~/pathto_metadata.csv",header=T,row.names=1)
-
-# Impose inclusion/exclusion criteria
-map2<-subset(map1,age_years>=20&age_years<=80&antibiotic_history<2&ibd==0&diabetes==0&bmi>=12.5&bmi<=40)
+# Impose inclusion/exclusion criteria, following are, as example, those for diabetes. These differ from special disease cohorts as defined in methods (IBD, ASD) and from remaining diseases. Namely, BMI is limited to 40 for other diseases and diabetics are excluded.
+map2<-subset(map1,age_years>=20&age_years<=80&antibiotic_history<2&ibd==0&bmi>=12.5)
 map2<-map2[rowSums(map2[,c("country_Canada","country_USA","country_United.Kingdom")])>0,]
+
+# Define number of cohort permutations to create
+nperms<-25
+
+# Define maximum cohort size (total number of cases and controls)
+maxcohortcutoff<-1000
+
+tab<-read.csv("~/Downloads/merged2_s50_otus_mc01p.csv",header=T,row.names=1)
 
 # Remove subjects not in OTU/ASV table
 map2<-map2[rownames(map2)%in%colnames(tab),]
@@ -66,39 +61,35 @@ map2$sugary_sweets_frequency <-scale(map2$sugary_sweets_frequency)
 map2$salted_snacks_frequency <-scale(map2$salted_snacks_frequency)
 map3<-map2[,c("latitude","longitude","sex","age_years","bmi", "alcohol_frequency","vegetable_frequency", "milk_cheese_frequency", "meat_eggs_frequency","bowel_movement_quality","whole_grain_frequency","sugary_sweets_frequency","salted_snacks_frequency")]
 locvars<-c("longitude","latitude")
-sub1<-subset(fishwilx2,dis== cohortnames[h])
-varsthatdiffer<-sub1[sub1$BH_qval=="mismatched","variablename"]
-map3<-map2[,c(locvars,varsthatdiffer)]
+
 map3<-map3[!is.na(rowSums(map3)),]
 
-vars<-c("matched","unmatched")
 
-justvars<-colnames(map3)[!colnames(map3)%in%c("longitude","latitude")]
+varsthatdiffer<-fishwilx2[fishwilx2 $matchstat =="mismatched","variablename"]
+map4<-map3[,c(locvars,as.vector(varsthatdiffer))]
 
-allmet2<-allmetadat[rownames(map3),]
+
+matchstat<-c("matched","unmatched")
+
+justvars<-colnames(map4)[!colnames(map4)%in%c("longitude","latitude")]
+
+allmet2<-allmetadat[rownames(map4),]
 
 # Remove all subjects not reporting information for given condition of interest
-allnonnas<-rownames(allmet2[!is.na(allmet2[, dis2[h]]==0),])
-
-# Create mapping file with just location metadata, randomize order of all subjects
-tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars)]
-
-# Create Euclidean distance matrix based on location metadata
-eudist<-dist(tempmap[,],method="euclidean")
-eu2<-as.matrix(eudist)
+allnonnas<-rownames(allmet2[!is.na(allmet2[, "diabetes"]==0),])
 
 
-# target1<-targetu[targetu%in%rownames(map3)]
-
-adonmat<-data.frame(matrix(nrow=length(vars)*nperms,ncol=10))
+adonmat<-data.frame(matrix(nrow=length(matchstat)*nperms,ncol=10))
 colnames(adonmat)<-colnames(adonmatall)
-for(j in 1:length(vars))
+for(j in 1:length(matchstat))
 {
-  if(vars[j]=="unmatched")
+  if(matchstat[j]=="unmatched")
 	  {
-	    tempmap<-map3[allnonnas,locvars]	
-	  }	else if(vars[j]=="matched")	{
-	    tempmap<-map3[allnonnas,c(locvars, justvars)]
+	    # Create mapping file with just location metadata, randomize order of all subjects
+		tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars)]
+	  }	else if(matchstat[j]=="matched")	{
+	  	# Create mapping file with all mismatched microbioa-confounding variables and location variables, while randomizing order of all subjects
+	    tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars, justvars)]
 	  }
 	  
 # Create Euclidean distance matrix based on given metadata
@@ -107,8 +98,6 @@ eu2<-as.matrix(eudist)
 
 for (l in 1:nperms)
    {
-    # Select only case subjects with matching variable metadata in mapping file
-	target1b<-targetu[targetu%in%rownames(map3)]
   	# Randomize order of cases, and impose maximum cohort limit (limiting number of cases, hence maxcohortcutoff/2)
   	target1<-sample(target1b,size=min(length(target1b),maxcohortcutoff/2))
   	
@@ -132,19 +121,14 @@ for (l in 1:nperms)
       ftab[i+length(target1),1]<-randcont
     }
 
-rownames(ftab)<-ftab[,"sampID"]
-write.csv(ftab,file=paste(outpath,as.character(cohortnames[h]),"-", vars[j],"-",l,".csv",sep=""))
-tab2<-tab[,colnames(tab)%in%rownames(ftab)]
+write.csv(ftab,file=paste(outpathcohorts,"diabetes-", matchstat[j],"-",l,".csv",sep=""),row.names=F)
+tab2<-tab[,colnames(tab)%in%ftab[,1]]
 
-# Take out samples from mapping file that are not in OTU table
-ftab2<-ftab[rownames(ftab)%in%colnames(tab2),]
-tab3<-tab2[,colnames(tab2)%in%rownames(ftab2)]
-
-tOTUtab<-t(tab3)
+tOTUtab<-t(tab2)
 
 canb<-as.matrix(dist(tOTUtab, method = "canberra", diag = T, upper = T))
 canb3<-canb[order(rownames(canb)),order(colnames(canb))]
-ftab2 <-as.data.frame(ftab2[order(rownames(ftab2)),])
+ftab2 <-as.data.frame(ftab[order(ftab[,1]),])
 formu<-formula(paste("canb3 ~ target"))
 MDSadonis <-adonis(formu, data= ftab2, permutations=adonperms)
 
@@ -155,22 +139,19 @@ MDSadonis <-adonis(formu, data= ftab2, permutations=adonperms)
     adonmat[k,4]<-MDSadonis$aov.tab[1,4]
     adonmat[k,5]<-MDSadonis$aov.tab[1,5]
     adonmat[k,6]<-MDSadonis$aov.tab[1,6]
-    adonmat[k,7]<-vars[j]
+    adonmat[k,7]<-matchstat[j]
     adonmat[k,8]<-nrow(canb3)
     adonmat[k,9]<-toString(colnames(tempmap))
-	adonmat[k,10]<-as.character(cohortnames[h])
+	adonmat[k,10]<-as.character("diabetes")
 	  } 
 }
 
 adonmatall<-rbind(adonmatall,adonmat)
 
 
-write.table(adonmat,file=paste(outpathadon, cohortnames[h],"-adonis_results.csv",sep=""),sep=",",col.names=NA)
+write.table(adonmat,file=paste(outpathadon,"diabetes-adonis_results.csv",sep=""),sep=",",col.names=NA)
 
 adonsum<-aggregate(adonmat[,"Fstat"], list(adonmat$sampIDparam), mean)
 adonmat $sampIDparam = factor(adonmat $sampIDparam, adonsum[order(adonsum[,2]),1])
 ggplot(adonmat,aes(x= sampIDparam,y=Fstat)) + theme_bw() +geom_boxplot(fatten = NULL,outlier.size=-1) + stat_summary(fun.y = mean, geom = "errorbar", aes(ymax = ..y.., ymin = ..y..), width = 0.75, size = 1, linetype = "solid") +geom_jitter(height=0,width=0.2,alpha=0.4) +theme(axis.text.x = element_text(angle = 90, hjust =1,vjust=0.3))
-ggsave(filename=paste(outpathadon, cohortnames[h],"-adonis.pdf",sep=""),height= 3.5,width=4.7)
-}
-
-write.csv(adonmatall,file=paste(outpathadon,"/alldis.csv",sep=""))
+ggsave(filename=paste(outpathadon, "diabetes-adonis.pdf",sep=""),height= 3.5,width=3)
