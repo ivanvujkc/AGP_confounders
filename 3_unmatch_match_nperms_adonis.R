be995362cd20ece6c8142ab28e85c216980b6654
ivanvujkc <ivan.vu.cv@gmail.com>
2020-06-09 14:30:12 -0400

    Update 3_unmatch_match_nperms_adonis.R

--- 3_unmatch_match_nperms_adonis.R
+++ 3_unmatch_match_nperms_adonis.R
@@ -68,13 +68,12 @@ map3<-map3[!is.na(rowSums(map3)),]
 varsthatdiffer<-fishwilx2[fishwilx2 $matchstat =="mismatched","variablename"]
 map4<-map3[,c(locvars,as.vector(varsthatdiffer))]
 
-
 matchstat<-c("matched","unmatched")
 
-justvars<-colnames(map4)[!colnames(map4)%in%c("longitude","latitude")]
-
+# calculate scaling-adjustment factor for each metadata variable that differs between cases and controls, based on extent to which they differed (log20 Benjamini-Hochberg Q value divided by nroot of total variables to match for)
 correc<-as.data.frame(cbind(var= as.character(fishwilx2[fishwilx2 $variablename%in%varsthatdiffer,"variablename"]), correction=(-log(fishwilx2[fishwilx2 $variablename%in%varsthatdiffer,"BH_qval"],base=20))^(1/(length(varsthatdiffer)+2))))
 
+# implement scaling-adjustment on centered, scaled metadata variables
 correc[,2]<-as.numeric(as.vector(correc[,2]))
 for (i in 1:nrow(correc))
 {
@@ -97,125 +96,7 @@ for(j in 1:length(matchstat))
 		tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars)]
 	  }	else if(matchstat[j]=="matched")	{
 	  	# Create mapping file with all mismatched microbioa-confounding variables and location variables, while randomizing order of all subjects
-	    tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars, justvars)]
-	  }
-	  
-# Create Euclidean distance matrix based on given metadata
-eudist<-dist(tempmap[,],method="euclidean")
-eu2<-as.matrix(eudist)
-
-for (l in 1:nperms)
-   {
-  	# Randomize order of cases, and impose maximum cohort limit (limiting number of cases, hence maxcohortcutoff/2)
-  	target1<-sample(target1b,size=min(length(target1b),maxcohortcutoff/2))
-  	
-  	# Create matrix containing names of all cases, dimensions to accommodate one control per case, and annotated by case/control status
-  	ftab<-matrix(nrow=length(target1)*2,ncol=2)
-    colnames(ftab)<-c("sampID","target")
-    ftab[1:length(target1),1]<-target1
-    ftab[1:length(target1),2]<-"case"
-    ftab[(length(target1)+1):(length(target1)*2),2]<-"control"
-    for (i in 1:length(target1))
-    {
-      sampname<-target1[i]
-      # Rank controls by shortest Euclidean distance to case subject
-      contsinord<-colnames(eu2[,order(eu2[sampname,])])
-      # Remove from consideration all cases and previously selected controls
-      contsinord2<-contsinord[!contsinord%in%ftab[,1]]
-      # Select top 5 most similar controls for this given case
-      topconts <-contsinord2[1:5]
-      # Randomly select one from top 5 most similar controls
-      randcont<-sample(topconts,size=1)
-      ftab[i+length(target1),1]<-randcont
-    }
-
-write.csv(ftab,file=paste(outpathcohorts,"diabetes-", matchstat[j],"-",l,".csv",sep=""),row.names=F)
-tab2<-tab[,colnames(tab)%in%ftab[,1]]
-
-tOTUtab<-t(tab2)
-
-canb<-as.matrix(dist(tOTUtab, method = "canberra", diag = T, upper = T))
-canb3<-canb[order(rownames(canb)),order(colnames(canb))]
-ftab2 <-as.data.frame(ftab[order(ftab[,1]),])
-formu<-formula(paste("canb3 ~ target"))
-MDSadonis <-adonis(formu, data= ftab2, permutations=adonperms)
-
-    k<-(j-1)*nperms+l
-    adonmat[k,1]<-MDSadonis$aov.tab[1,1]
-    adonmat[k,2]<-MDSadonis$aov.tab[1,2]
-    adonmat[k,3]<-MDSadonis$aov.tab[1,3]
-    adonmat[k,4]<-MDSadonis$aov.tab[1,4]
-    adonmat[k,5]<-MDSadonis$aov.tab[1,5]
-    adonmat[k,6]<-MDSadonis$aov.tab[1,6]
-    adonmat[k,7]<-matchstat[j]
-    adonmat[k,8]<-nrow(canb3)
-    adonmat[k,9]<-toString(colnames(tempmap))
-	adonmat[k,10]<-as.character("diabetes")
-	  } 
-}
-
-adonmatall<-rbind(adonmatall,adonmat)
-
-
-write.table(adonmat,file=paste(outpathadon,"diabetes-adonis_results.csv",sep=""),sep=",",col.names=NA)
-
-adonsum<-aggregate(adonmat[,"Fstat"], list(adonmat$sampIDparam), mean)
-adonmat $sampIDparam = factor(adonmat $sampIDparam, adonsum[order(adonsum[,2]),1])
-ggplot(adonmat,aes(x= sampIDparam,y=Fstat)) + theme_bw() +geom_boxplot(fatten = NULL,outlier.size=-1) + stat_summary(fun.y = mean, geom = "errorbar", aes(ymax = ..y.., ymin = ..y..), width = 0.75, size = 1, linetype = "solid") +geom_jitter(height=0,width=0.2,alpha=0.4) +theme(axis.text.x = element_text(angle = 90, hjust =1,vjust=0.3))
-ggsave(filename=paste(outpathadon, "diabetes-adonis.pdf",sep=""),height= 3.5,width=3)# Define number of cohort permutations to create
-nperms<-25
-
-
-tab<-read.csv("~/Downloads/merged2_s50_otus_mc01p.csv",header=T,row.names=1)
-
-# Remove subjects not in OTU/ASV table
-map2<-map2[rownames(map2)%in%colnames(tab),]
-
-# Scale and center all matching variables
-map2$latitude<-scale(map2$latitude)
-map2$longitude<-scale(map2$longitude)
-map2$sex<-scale(map2$sex)
-map2$age_years<-scale(map2$age_years)
-map2$bmi<-scale(map2$bmi)
-map2$alcohol_frequency<-scale(map2$alcohol_frequency)
-map2$vegetable_frequency<-scale(map2$vegetable_frequency)
-map2$milk_cheese_frequency<-scale(map2$milk_cheese_frequency)
-map2$meat_eggs_frequency<-scale(map2$meat_eggs_frequency)
-map2$bowel_movement_quality <-scale(map2$bowel_movement_quality)
-map2$whole_grain_frequency <-scale(map2$whole_grain_frequency)
-map2$sugary_sweets_frequency <-scale(map2$sugary_sweets_frequency)
-map2$salted_snacks_frequency <-scale(map2$salted_snacks_frequency)
-map3<-map2[,c("latitude","longitude","sex","age_years","bmi", "alcohol_frequency","vegetable_frequency", "milk_cheese_frequency", "meat_eggs_frequency","bowel_movement_quality","whole_grain_frequency","sugary_sweets_frequency","salted_snacks_frequency")]
-locvars<-c("longitude","latitude")
-
-map3<-map3[!is.na(rowSums(map3)),]
-
-
-varsthatdiffer<-fishwilx2[fishwilx2 $matchstat =="mismatched","variablename"]
-map4<-map3[,c(locvars,as.vector(varsthatdiffer))]
-
-
-matchstat<-c("matched","unmatched")
-
-justvars<-colnames(map4)[!colnames(map4)%in%c("longitude","latitude")]
-
-allmet2<-allmetadat[rownames(map4),]
-
-# Remove all subjects not reporting information for given condition of interest
-allnonnas<-rownames(allmet2[!is.na(allmet2[, "diabetes"]==0),])
-
-
-adonmat<-data.frame(matrix(nrow=length(matchstat)*nperms,ncol=10))
-colnames(adonmat)<-colnames(adonmatall)
-for(j in 1:length(matchstat))
-{
-  if(matchstat[j]=="unmatched")
-	  {
-	    # Create mapping file with just location metadata, randomize order of all subjects
-		tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars)]
-	  }	else if(matchstat[j]=="matched")	{
-	  	# Create mapping file with all mismatched microbioa-confounding variables and location variables, while randomizing order of all subjects
-	    tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars, justvars)]
+	    tempmap<-map3[sample(allnonnas,size=length(allnonnas)),c(locvars, varsthatdiffer)]
 	  }
 	  
 # Create Euclidean distance matrix based on given metadata
